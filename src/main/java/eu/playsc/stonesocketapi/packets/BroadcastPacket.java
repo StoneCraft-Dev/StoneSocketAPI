e5e1faf77c0c6fb0e119350994318016135aae1a
Justin <Justin.Schweiger@t-online.de>
2023-10-02 13:18:00 +0200

    fixed an oopsie
    Took 3 minutes

--- src/main/java/eu/playsc/stonesocketapi/packets/BroadcastPacket.java
+++ src/main/java/eu/playsc/stonesocketapi/packets/BroadcastPacket.java
@@ -1,16 +1,19 @@
 package eu.playsc.stonesocketapi.packets;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class BroadcastPacket implements Packet {
 	private final String identifier;
-	@Getter
-	@Setter
 	private String message;
 
 	public BroadcastPacket(String identifier) {
 		this.identifier = identifier;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
 	@Override
