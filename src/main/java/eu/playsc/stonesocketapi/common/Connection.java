8823dbd8b8207680aeaceeee9c8aae0fac24958d
Justin <Justin.Schweiger@t-online.de>
2023-10-03 19:47:29 +0200

    dude idfk anymore
    Took 4 minutes

--- src/main/java/eu/playsc/stonesocketapi/common/Connection.java
+++ src/main/java/eu/playsc/stonesocketapi/common/Connection.java
@@ -4,6 +4,7 @@ import eu.playsc.stonesocketapi.Logger;
 import eu.playsc.stonesocketapi.packets.Packet;
 import eu.playsc.stonesocketapi.server.ConnectionManager;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
@@ -57,8 +58,16 @@ public class Connection {
 
 	public void sendPacket(Packet packet) {
 		try {
-			outputStream.writeObject(packet);
-			outputStream.flush();
+			ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
+			ObjectOutputStream objOut = new ObjectOutputStream(byteOutStream);
+			objOut.writeObject(packet);
+			byte[] data = byteOutStream.toByteArray();
+			objOut.close();
+			byteOutStream.close();
+			synchronized (outputStream) {
+				outputStream.writeObject(data);
+				outputStream.flush();
+			}
 		} catch (IOException e) {
 			Logger.error(e);
 			if (protocol.getListener() != null)
